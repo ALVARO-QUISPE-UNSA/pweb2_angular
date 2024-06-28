f98d3a8b3c80b94e1f077c3f6ccac4daf90ffc87
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-06-28 09:04:07 -0500

    Se añadió los atributos a mi componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'my-dream-app';
+  name = "Álvaro Raúl Quispe Condori";
+  email = "aquispecondo@unsa.edu.pe";
+  webpage = "https://github.com/ALVARO-QUISPE-UNSA";
 }
