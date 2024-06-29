bbbb5631285035a2ff143aa1c7890767b378ac74
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-06-28 20:20:52 -0500

    Se trató de añadir un componente de user pero ocurre error, no se realiza el for

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent implements OnInit{
+  @Input() nameUser:string = '';
+  constructor() {}
+  ngOnInit(): void {
+    console.log(`El nombre del usuario es: ${this.nameUser}`);
+  }
+}
