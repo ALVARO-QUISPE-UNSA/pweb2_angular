2d4f738729f6ef897e3fd785966780b8af90c21b
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-07-04 20:39:54 -0500

    Se eliminó la interface de Observable al retornar los datos

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Post } from './Post';
-import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +11,7 @@ export class DataService {
   constructor(private httpClient:HttpClient) {
     console.log("Service working...");
   }
-  getData(): Observable<Post[]> {
+  getData(){
     return this.httpClient.get<Post[]>("https://jsonplaceholder.typicode.com/posts");
   }
 }
